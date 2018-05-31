5f4fc95488d46421fd229aa736aa8fc654ed8245
ret123 <rathish83@gmail.com>
2018-05-31 21:56:06 +0530

    Materialize css and header style implemented

---
+++ client/src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BrowserRouter,Route} from 'react-router-dom';
+import Header from './Header';
+
+
+const DashBoard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
+const App = () => {
+    return (
+        <div>
+           <BrowserRouter>
+            <div>
+                <Header />
+                <Route exact path="/" component={Landing} />
+                <Route exact path="/surveys" component={DashBoard} />
+                <Route  path="/survey/new" component={SurveyNew} />
+            </div>
+           </BrowserRouter>
+        </div>
+    );
+};
+export default App;
