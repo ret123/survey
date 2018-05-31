aa39f533b242df60a69048c3bf31d6b1da91f1f7
ret123 <rathish83@gmail.com>
2018-05-31 13:18:59 +0530

    Auth flow done

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    MONGO_URI: process.env.MONGO_URI,
+    COOKIE_KEY: process.env.COOKIE_KEY
+    }
